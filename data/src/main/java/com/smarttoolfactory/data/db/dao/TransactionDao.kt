7065bcf0f66b938b10a0a453498d6de4c8be7cfd
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-02-12 13:02:10 +0300

    add TransactionDao functions

--- data/src/main/java/com/smarttoolfactory/data/db/dao/TransactionDao.kt
+++ data/src/main/java/com/smarttoolfactory/data/db/dao/TransactionDao.kt
@@ -1,8 +1,57 @@
 package com.smarttoolfactory.data.db.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import com.smarttoolfactory.data.model.TransactionEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TransactionDao {
 
+    /**
+     * get list of [TransactionEntity]
+     */
+    @Query("SELECT * FROM table_transaction")
+    suspend fun getTransactionList(): List<TransactionEntity>
+
+    @Transaction
+    @Query("SELECT * FROM table_transaction")
+    suspend fun getTransactionMap(): Map<String, String> {
+        val transactionMap = hashMapOf<String, String>()
+        getTransactionList().forEach { transactionEntity: TransactionEntity ->
+            transactionMap[transactionEntity.key] = transactionEntity.value
+        }
+
+        return transactionMap
+    }
+
+    /**
+     * Insert a transaction
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTransaction(entity: TransactionEntity)
+
+    @Transaction
+    suspend fun insertTransactions(map: Map<String, String>) {
+        map.forEach { (key, value) ->
+            insertTransaction(TransactionEntity(key, value))
+        }
+    }
+
+    /**
+     * Delete [TransactionEntity]
+     */
+    @Delete
+    suspend fun deleteTransaction(entity: TransactionEntity)
+
+    /**
+     * Update an existing [TransactionEntity]
+     */
+    @Update
+    suspend fun updateTransaction(entity: TransactionEntity): Int
 }
--- data/src/main/java/com/smarttoolfactory/data/model/TransactionEntity.kt
+++ data/src/main/java/com/smarttoolfactory/data/model/TransactionEntity.kt
@@ -7,7 +7,7 @@ import java.util.UUID
 @Entity(tableName = "table_transaction")
 data class TransactionEntity(
     @PrimaryKey
-    val noteId: String = UUID.randomUUID().toString(),
     val key: String,
-    val value: String
+    val value: String,
+    val uuid: String = UUID.randomUUID().toString()
 )
