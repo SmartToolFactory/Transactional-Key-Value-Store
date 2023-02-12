dfd732903bc302fc7ac9c900d96ec75fb6a745a9
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-02-12 15:45:59 +0300

    bind TransactionRepository with implementation di

--- data/src/main/java/com/smarttoolfactory/data/di/DataBaseModule.kt
+++ data/src/main/java/com/smarttoolfactory/data/di/DataBaseModule.kt
@@ -4,11 +4,26 @@ import android.app.Application
 import androidx.room.Room
 import com.smarttoolfactory.data.db.AppDatabase
 import com.smarttoolfactory.data.db.dao.TransactionDao
+import com.smarttoolfactory.data.repository.TransactionRepository
+import com.smarttoolfactory.data.repository.TransactionRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module(
+    includes = [
+        DatabaseModule::class
+    ]
+)
+interface DataModule {
+    @Singleton
+    @Binds
+    fun bindRepository(repository: TransactionRepositoryImpl): TransactionRepository
+}
 
 @InstallIn(SingletonComponent::class)
 @Module
