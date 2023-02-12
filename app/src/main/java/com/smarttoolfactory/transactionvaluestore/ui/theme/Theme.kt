832b68dd5edf04dd98bb51509e1becb8520f31bf
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-02-13 00:50:13 +0300

    change status bar color

--- app/src/main/java/com/smarttoolfactory/transactionvaluestore/ui/theme/Theme.kt
+++ app/src/main/java/com/smarttoolfactory/transactionvaluestore/ui/theme/Theme.kt
@@ -3,14 +3,9 @@ package com.smarttoolfactory.transactionvaluestore.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -57,7 +52,6 @@ fun TransactionKeyValueStoreTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
